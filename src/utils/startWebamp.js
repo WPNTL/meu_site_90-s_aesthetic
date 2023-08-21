210fa5dacb8b02fcd2c1924c9485bce0c1bd5202
WPNTL <wesleypuntel@gmail.com>
2023-08-20 23:34:27 -0300

    startWebamp.js review
    startWebamp.js review

--- src/utils/startWebamp.js
+++ src/utils/startWebamp.js
@@ -1,42 +1,42 @@
-import Webamp from 'webamp'
+import Webamp from "webamp";
 
 const startWebamp = () => {
-    if (Webamp.browserIsSupported()) {
-        const webamp = new Webamp({
-            initialTracks: [
-                {
-                    metaData: {
-                        artist: "Rick Astley",
-                        title: "Never Gonna Give You Up"
-                    },
-                    url: `${process.env.PUBLIC_URL}/never-gonna-give-you-up.mp3`,
-                    duration: 213
-                },
-                {
-                    metaData: {
-                        artist: "Haddaway",
-                        title: "What is Love?"
-                    },
-                    url: `${process.env.PUBLIC_URL}/what-is-love.mp3`,
-                    duration: 267
-                },
-                {
-                    metaData: {
-                        artist: "Darude",
-                        title: "Sandstorm"
-                    },
-                    url: `${process.env.PUBLIC_URL}/sandstorm.mp3`,
-                    duration: 313
-                }
-            ]
-        });
+  if (Webamp.browserIsSupported()) {
+    const webamp = new Webamp({
+      initialTracks: [
+        {
+          metaData: {
+            artist: "Rick Astley",
+            title: "Never Gonna Give You Up",
+          },
+          url: `${process.env.PUBLIC_URL}/never-gonna-give-you-up.mp3`,
+          duration: 213,
+        },
+        {
+          metaData: {
+            artist: "Haddaway",
+            title: "What is Love?",
+          },
+          url: `${process.env.PUBLIC_URL}/what-is-love.mp3`,
+          duration: 267,
+        },
+        {
+          metaData: {
+            artist: "Darude",
+            title: "Sandstorm",
+          },
+          url: `${process.env.PUBLIC_URL}/sandstorm.mp3`,
+          duration: 313,
+        },
+      ],
+    });
 
-        webamp.onClose(() => {
-            webamp.dispose();
-        })
+    webamp.onClose(() => {
+      webamp.dispose();
+    });
 
-        webamp.renderWhenReady(document.getElementById('winamp-container'));
-    }
-}
+    webamp.renderWhenReady(document.getElementById("winamp-container"));
+  }
+};
 
-export { startWebamp }
+export { startWebamp };
